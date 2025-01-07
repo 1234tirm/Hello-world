e472c2a68a0b9afa5186d9c48e044fb6efd51e37
1234tirm <ark290991@gmail.com>
2025-01-07 11:58:39 +0530

    I changed this.

---
+++ add.py
@@ -0,0 +1,10 @@
+checking..
+
+##4 types of git status
+# Untracked: Git does not know about the file, since it's not committed. New file.
+# Modified: We made changes in the earlier committed file.
+# Unmodified: No change in file.
+# Staged: File ready to be committed.
+
+# This is a dummy python file.
+#feef
